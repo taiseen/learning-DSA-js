a23340a8a4e6f9ce830ea50a74e38ed1bdf1b225
taiseen <taiseen.cse@gmail.com>
2023-05-18 01:07:23 +0600

    feat: recursion practice

---
+++ Algorithm/Recursion/01.Basic.js
@@ -0,0 +1,52 @@
+// 17 - May - 2023
+
+// Recursive function OR Recursion
+
+
+const recursionFun = () => {
+
+    console.log('Recursion - Infinite Loop');
+
+    return recursionFun();
+}
+
+// recursionFun(); // infinite loop happen, when have no base case...
+
+
+// 🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨
+// 🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨
+// 🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨
+
+
+const justNumberLoop = (number) => {
+
+    console.log(number);
+
+    return number === 1                     // <=== base case | stopper 🟩🟩🟩
+        ? 1
+        : justNumberLoop(number - 1)
+}
+
+// justNumberLoop(5); // no infinite loop happen, because have a base case...
+
+
+// 🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨
+// 🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨
+// 🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨
+
+
+// 1 * 2 * 3 * 4 * 5 ==> 120
+
+const factorial = (number) => {
+
+    return number === 1                     // <=== base case | stopper 🟩🟩🟩
+        ? 1
+        : number * factorial(number - 1)
+}
+
+console.log(factorial(5));
+
+
+// 🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨
+// 🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨
+// 🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨
---
+++ Algorithm/Recursion/02.Fibonacci.js
@@ -0,0 +1,44 @@
+// Fibonacci Series
+
+
+// print particular series number base on position number
+
+const fibonacci = (num) => {
+
+    return num <= 1
+        ? 1
+        : fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+const positionNumber = 7;
+
+// console.log(fibonacci(positionNumber));
+
+
+// 🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨
+// 🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨
+// 🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨
+
+
+// print total series number list base on position number
+
+const fibonacciSeries = (position, series = [0, 1]) => {
+
+    const totalNumbers = series.length;
+    const _1st = series[totalNumbers - 1];
+    const _2nd = series[totalNumbers - 2];
+    const updatedSeries = [...series, _1st + _2nd];
+
+    return position <= 1
+        ? series
+        : fibonacciSeries(position - 1, updatedSeries);
+};
+
+const position = 6;
+
+console.log(fibonacciSeries(position));
+
+
+// 🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨
+// 🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨
+// 🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨
