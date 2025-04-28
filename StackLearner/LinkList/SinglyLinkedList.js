66e0557993fe4846bb7a4fb4bfc53cbd71716736
taiseen <taiseen.cse@gmail.com>
2025-04-28 19:41:36 +0600

    feat: SinglyLinkedList prepend and toSring

---
+++ StackLearner/LinkList/SinglyLinkedList.js
@@ -0,0 +1,41 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+        this.length = 0;
+    }
+
+    prepend(value) {
+        const newNode = new Node(value);
+        newNode.next = this.head; // new node points to the current head
+        this.head = newNode; // head update to new node
+        this.length++;
+    }
+
+    toString() {
+        let current = this.head;
+        let result = '';
+
+        while (current) {
+            const arrow = current.next ? ' -> ' : '';
+            result += current.value + arrow;
+            current = current.next;
+        }
+        return result;
+    }
+}
+
+const list = new SinglyLinkedList();
+list.prepend(13);
+list.prepend(80);
+list.prepend(57);
+list.prepend(60);
+
+
+console.log(list.toString()); // 4 -> 3 -> 2 -> 1
